beb8353997f17a24c9602503307bd693aad2d2dc
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2020-10-10 19:56:33 +0300

    Initial Commit

--- res/js/script.js
+++ res/js/script.js
@@ -1,21 +1,55 @@
+$(function () {
 
-$(function() {
-
-    $('.pill').click(function() {
+    $('.pill').click(function () {
         switchTab($(this).attr('data-target'))
-    })
+    });
 
-})
+    loadUserInfo()
+        .then(function (response) {
+            user = new User(
+                response.firstname,
+                response.lastname,
+                response.birthdate,
+                response.faculty
+            );
+
+            displayUserInfo(user)
+        })
+        .catch(function () {
+            alert('Error loading user info')
+        });
+
+});
+
+function displayUserInfo(user) {
+    $('#profile #name').text(user.firstname + " " + user.lastname);
+    $('#profile #birthdate').text(user.birthdate );
+    $('#profile #faculty').text(user.faculty );
+}
+
+function loadUserInfo() {
+    return $.get(
+        {
+            url: './data/user.json',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    );
+}
 
 function switchTab(id) {
-    $('.tab').each(function(){
+    $('.tab').each(function () {
         if ($(this).attr('id') === id) {
             $(this).addClass('active')
         } else {
             $(this).removeClass('active')
         }
     });
-    $('.pill').each(function(){
+    $('.pill').each(function () {
         if ($(this).attr('data-target') === id) {
             $(this).addClass('active')
         } else {
