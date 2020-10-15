3ae0f6667ec39c2afead67379b8081860972aefb
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2020-10-15 13:39:29 +0300

    Solution

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,5 @@
+let courses = [];
+
 $(function () {
 
     $('.pill').click(function () {
@@ -6,7 +8,7 @@ $(function () {
 
     loadUserInfo()
         .then(function (response) {
-            user = new User(
+            let user = new User(
                 response.firstname,
                 response.lastname,
                 response.birthdate,
@@ -19,7 +21,55 @@ $(function () {
             alert('Error loading user info')
         });
 
+
+    loadCourses()
+        .then(function (response) {
+            for (let course of response) {
+                courses.push(new Course(course.title, course.semester, course.grade))
+            }
+
+            displayCourses()
+        })
+        .catch(function () {
+            alert('Error loading courses info')
+        });
+
+    $('#add-course-button').click(function () {
+        $('#add-course').toggle()
+    })
+
+    $('#save-course').click(function () {
+        let title       = $('#title').val()
+        let semester    = $('#semester').val()
+        let grade       = $('#grade').val()
+
+        if (!title || !semester || !grade) {
+            alert("Please fill in the form")
+            return;
+        }
+
+        let course = new Course(title, semester, grade)
+
+        courses.push(course)
+
+        displayCourses()
+
+        closeCourseForm()
+
+    })
+
+    $('#cancel-course').click(function () {
+        closeCourseForm()
+    })
+
 });
+
+function closeCourseForm() {
+    $('#title').val("")
+    $('#semester').val("")
+    $('#grade').val("")
+    $('#add-course').toggle()
+}
 
 function displayUserInfo(user) {
     $('#profile #name').text(user.firstname + " " + user.lastname);
@@ -27,10 +77,78 @@ function displayUserInfo(user) {
     $('#profile #faculty').text(user.faculty );
 }
 
+function displayCourses() {
+
+    let gpa = calculateGPA()
+    $('#gpa strong').text(gpa)
+
+    $('table#courses tbody').empty()
+    let i = 1;
+    for (let course of courses) {
+
+        let tr = $('<tr>')
+        let number = $('<td>').text(i)
+        let title = $('<td>').text(course.title)
+        let semester = $('<td>').text(course.semester)
+        let grade = $('<td>').text(course.grade)
+
+        tr.append(number)
+        tr.append(title)
+        tr.append(semester)
+        tr.append(grade)
+
+        $('table#courses tbody').append(tr)
+
+        i++
+    }
+}
+
+function calculateGPA() {
+
+    let result = 0;
+
+    for (let course of courses) {
+        if (course.grade > 90) {
+            result += 4
+        }
+        else if (course.grade > 80) {
+            result += 3
+        }
+        else if (course.grade > 70) {
+            result += 2
+        }
+        else if (course.grade > 60) {
+            result += 1
+        }
+        else if (course.grade > 50) {
+            result += 0.5
+        } else {
+            result += 0
+        }
+    }
+
+    return result / courses.length
+
+}
+
 function loadUserInfo() {
     return $.get(
         {
-            url: 'data/user.json',
+            url: 'https://private-a62dc2-wad20lab7.apiary-mock.com/users/1',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    );
+}
+
+function loadCourses() {
+    return $.get(
+        {
+            url: 'https://private-a62dc2-wad20lab7.apiary-mock.com/users/1/courses',
             success: function (response) {
                 return response;
             },
